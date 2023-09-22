11f2b8dfc70b0753a706cb9c8adc5f02875f042f
Youssef <220238@fppiramide.com>
2023-09-22 14:16:27 +0200

    ..

---
+++ src/main/java/com/example/Actividad1/Actividad1Application.java
@@ -0,0 +1,13 @@
+package com.example.Actividad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Actividad1/Actividad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Actividad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
