1b013028b3dca63bb1ee9255f88025e8b7bc74db
Youssef <220238@fppiramide.com>
2023-09-26 14:00:38 +0200

    Acrividad1

--- src/main/java/com/example/Actividad1/Controller/Controller.java
+++ src/main/java/com/example/Actividad1/Controller/Controller.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 @org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/variable/{x}")
@@ -37,13 +40,59 @@ public class Controller {
     }
     @GetMapping("/perfil")
     String perfil(Model model){
-        Persona persona = new Persona("Manuel",32);
+        Persona persona = new Persona("Manuel",17);
         persona.intereses.add("Pley");
         persona.intereses.add("Criptomonedas");
         persona.intereses.add("Messi");
 
         model.addAttribute("persona",persona);
         return "perfil";
+    }
+    @GetMapping("/array/{x}")
+    String array(Model model,@PathVariable Integer x){
+        Integer entero = x;
+        ArrayList<Integer> lista = new ArrayList<>();
+        int aux;
+
+        lista.add(2);
+        lista.add(1);
+        lista.add(7);
+        lista.add(23);
+        lista.add(5);
+
+        if (lista.contains(entero))
+            aux = 1;
+        else aux = 0;
+
+        model.addAttribute("entero",entero);
+        model.addAttribute("lista",lista);
+        model.addAttribute("aux",aux);
+
+        return "array";
+    }
+
+    @RequestMapping("/nuevoentero")
+    String nuevoentero(Model model){
+
+        return "crearentero";
+    }
+
+
+    @PostMapping("/lista")
+    String anyadiralista(Model model, Integer entero){
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        lista.add(2);
+        lista.add(1);
+        lista.add(7);
+        lista.add(23);
+        lista.add(5);
+
+        lista.add(entero);
+
+        Collections.sort(lista);
+        model.addAttribute("lista",lista);
+        return "lista";
     }
 }
 
